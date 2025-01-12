5ec96eed7888a3c2e37c1e88621849b485d7d14f
Madeline Kahn <mk-acct@outlook.com>
2025-01-12 02:56:17 -0500

    Split joke into different file

---
+++ Content/Misc/NoFalseAdvertisingShops.cs
@@ -0,0 +1,45 @@
+using static MaddieQoL.Common.Shorthands;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using MaddieQoL.Util;
+
+namespace MaddieQoL.Content.Misc;
+
+public sealed class NoFalseAdvertisingShops : GlobalNPC {
+	public override void ModifyShop(NPCShop shop) {
+		switch (shop.NpcType) {
+			case NPCID.Merchant:
+				ModifyMerchantShop(shop);
+				break;
+		}
+	}
+
+	private static void ModifyMerchantShop(NPCShop shop) {
+		if (!ModuleConf.enableMerchantShopPerDialogue) {return;}
+		shop.InsertAfter(ItemID.BugNet, new Item(ItemID.Umbrella) {
+			shopCustomPrice = Item.buyPrice(0, 0, 50, 0)
+		}, Condition.InRain);
+		shop.InsertBefore(ItemID.CopperPickaxe, ItemID.CopperShortsword);
+		shop.InsertAfter(ItemID.CopperAxe, ItemID.CopperHelmet, Condition.TimeDay);
+		shop.InsertAfter(ItemID.CopperHelmet, ItemID.CopperChainmail, Condition.TimeDay);
+		shop.InsertAfter(ItemID.CopperChainmail, ItemID.CopperGreaves, Condition.TimeDay);
+		shop.InsertAfter(ItemID.ThrowingKnife, ItemID.Lens, Condition.BloodMoon);
+		shop.Add(new Item(ItemID.DirtBlock) {
+			shopCustomPrice = Item.buyPrice(0, 0, 0, 2)
+		});
+	}
+
+	public override void ModifyActiveShop(NPC npc, string shopName, Item[] items) {
+		if (npc.type == NPCID.TravellingMerchant) {
+			ModifyTravelingMerchantActiveShop(items);
+		}
+	}
+
+	private static void ModifyTravelingMerchantActiveShop(Item[] items) {
+		if (!ModuleConf.enableMerchantShopPerDialogue) {return;}
+		ShopUtil.TryAddItemToActiveShop(items, new Item(ItemID.AngelStatue) {
+			shopCustomPrice = Item.buyPrice(0, 5, 0, 0)
+		});
+	}
+}
--- Content/Misc/Shops.cs
+++ Content/Misc/Shops.cs
@@ -1,52 +1,18 @@
-using static MaddieQoL.Common.Shorthands;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria;
-using MaddieQoL.Util;
 
 namespace MaddieQoL.Content.Misc;
 
 public sealed class MiscShops : GlobalNPC {
 	public override void ModifyShop(NPCShop shop) {
 		switch (shop.NpcType) {
-			case NPCID.Merchant:
-				ModifyMerchantShop(shop);
-				break;
 			case NPCID.Mechanic:
 				ModifyMechanicShop(shop);
 				break;
 		}
 	}
 
-	private static void ModifyMerchantShop(NPCShop shop) {
-		if (!ModuleConf.enableMerchantShopPerDialogue) {return;}
-		shop.InsertAfter(ItemID.BugNet, new Item(ItemID.Umbrella) {
-			shopCustomPrice = Item.buyPrice(0, 0, 50, 0)
-		}, Condition.InRain);
-		shop.InsertBefore(ItemID.CopperPickaxe, ItemID.CopperShortsword);
-		shop.InsertAfter(ItemID.CopperAxe, ItemID.CopperHelmet, Condition.TimeDay);
-		shop.InsertAfter(ItemID.CopperHelmet, ItemID.CopperChainmail, Condition.TimeDay);
-		shop.InsertAfter(ItemID.CopperChainmail, ItemID.CopperGreaves, Condition.TimeDay);
-		shop.InsertAfter(ItemID.ThrowingKnife, ItemID.Lens, Condition.BloodMoon);
-		shop.Add(new Item(ItemID.DirtBlock) {
-			shopCustomPrice = Item.buyPrice(0, 0, 0, 2)
-		});
-	}
-
 	private static void ModifyMechanicShop(NPCShop shop) {
 		shop.InsertAfter(ItemID.Switch, ModContent.ItemType<Items.Button>());
-	}
-
-	public override void ModifyActiveShop(NPC npc, string shopName, Item[] items) {
-		if (npc.type == NPCID.TravellingMerchant) {
-			ModifyTravelingMerchantActiveShop(items);
-		}
-	}
-
-	private static void ModifyTravelingMerchantActiveShop(Item[] items) {
-		if (!ModuleConf.enableMerchantShopPerDialogue) {return;}
-		ShopUtil.TryAddItemToActiveShop(items, new Item(ItemID.AngelStatue) {
-			shopCustomPrice = Item.buyPrice(0, 5, 0, 0)
-		});
 	}
 }
